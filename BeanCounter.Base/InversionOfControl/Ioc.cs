befd120191a6708f9a8c56c28ecd018ab42279f9
akj <akj@bbtsoftware.ch>
2017-07-14 19:30:17 +0200

    Unity package added, RuntimeChecks added, "Feature" added...

---
+++ BeanCounter.Base/InversionOfControl/Ioc.cs
@@ -0,0 +1,18 @@
+﻿namespace BeanCounter.Base.InversionOfControl
+{
+    /// <summary>
+    /// Provides access to the global <see cref="IIocContainer"/>.
+    /// </summary>
+    public static class Ioc
+    {
+        /// <summary>
+        /// Stores the global <see cref="IIocContainer"/> instance.
+        /// </summary>
+        private static readonly IIocContainer IocContainer = new UnityContainerWrapper();
+
+        /// <summary>
+        /// Gets the global <see cref="IIocContainer"/>.
+        /// </summary>
+        public static IIocContainer Container => IocContainer;
+    }
+}
---
+++ BeanCounter.Calculator/Data/Feature.cs
@@ -0,0 +1,26 @@
+﻿namespace BeanCounter.Calculator.Data
+{
+    /// <summary>
+    /// Represents a feature to be implemented.
+    /// </summary>
+    public class Feature
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Feature"/> class.
+        /// </summary>
+        public Feature(
+            decimal costOfDelayPerWeek,
+            decimal developmentDurationInDays)
+        {
+            this.CostOfDelayPerWeek = costOfDelayPerWeek;
+            this.DevelopmentDurationInDays = developmentDurationInDays;
+        }
+
+        /// <summary>
+        /// Gets the estimated cost of delay per week (in any arbitrary currency).
+        /// </summary>
+        public decimal CostOfDelayPerWeek { get; }
+
+        public decimal DevelopmentDurationInDays { get; }
+    }
+}
